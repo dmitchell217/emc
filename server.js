5d72f4294010fa97973f677314afbe2ccba2d2cc
david mitchell <david@swantide.com>
2023-07-15 17:59:48 -0400

    parcel-bundler

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.static('dist'));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
